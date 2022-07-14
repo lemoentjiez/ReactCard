598c6de634acac73f231009342813eae9702c7d7
Lemoentjiez <danthevip@gmail.com>
2022-07-15 02:50:45 +0300

    Mistake fixed

--- src/elements/MainContent.js
+++ src/elements/MainContent.js
@@ -9,7 +9,7 @@ export function MainContent() {
             <div>
                 <ul className="mainUl">
                     <li>Was first released in 2013</li>
-                    <li>War originally created by Jordan Walke</li>
+                    <li>Was originally created by Jordan Walke</li>
                     <li>Has well over 100K stars on GitHub</li>
                     <li>Is maintained by Facebook</li>
                     <li>Powers thousands entreprise apps, including mobile apps</li>
